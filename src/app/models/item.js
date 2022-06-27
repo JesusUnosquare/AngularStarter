21f3df831d5701c7f6a86eb845a03460d8553edb
Jesus Martinez <jesus.martinez@unosquare.com>
2022-06-27 11:29:47 -0500

    FirstCommit

---
+++ src/app/models/item.js
@@ -0,0 +1,12 @@
+export class Item{
+    id = 0;
+    title = '';
+    price = 0;
+    quantity = 0;
+    completed = false;
+    // id: number = 0;
+    // title: string = '';
+    // price: number = 0;
+    // quantity: number = 0;
+    // completed: boolean = false;
+}
